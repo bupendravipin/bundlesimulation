3fee1089147796464dac93ca8d037514bbdb22f4
bupendravipin <b.upendravipin@gmail.com>
2021-06-08 09:45:08 +0530

    added logger.info statement

--- app.py
+++ app.py
@@ -6,15 +6,19 @@ Created on May 31, 2021
 
 from flask import Flask, request, jsonify
 import pandas as pd
+from logger import *
 app = Flask(__name__)
 
+logger=logging_func()
 
-@app.route('/home')
+@app.route('/')
 def home():
+    logger.info('In home endpoint')
     return 'Hello World'
 
 @app.route('/simulate')
 def recommend():
+    logger.info('In simulate endpoint')
     df=pd.DataFrame([1,2,3],columns=['A'])
 #     url = 'https://raw.githubusercontent.com/bupendravipin/bundlesimulation/master/712034.xlsx'
 #     df = pd.read_excel(url, index_col=0)
